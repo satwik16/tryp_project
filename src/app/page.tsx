ad7c6b547aedbc60a2f429dc88cc9fe1322bab21
satwik16 <61792713+satwik16@users.noreply.github.com>
2023-07-17 13:32:01 +0530

    solved chakra UI error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
-import Image from "next/image";
-import styles from "./page.module.css";
 import "../app/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
 
 // @ts-ignore
 import TableComponent from "./TableComponent.tsx";

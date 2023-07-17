112206927355c15a3be17d3bc612ca3230427236
satwik16 <61792713+satwik16@users.noreply.github.com>
2023-07-17 13:39:25 +0530

    solved axios module not found error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import "../app/globals.css";
 
 // @ts-ignore
 import TableComponent from "./TableComponent.tsx";
-import { useEffect, useState } from "react";
-import axios from "axios";
+
 export default function Home() {
   return <TableComponent />;
 }

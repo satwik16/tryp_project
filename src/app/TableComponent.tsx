1da45c53b607dd423d51fd04894779f715dae4c7
satwik16 <61792713+satwik16@users.noreply.github.com>
2023-07-17 13:25:08 +0530

    update on axios dependency

--- src/app/TableComponent.tsx
+++ src/app/TableComponent.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
-import axios from "axios";
 import "../app/globals.css";
 const TableComponent = () => {
   type Country = {
